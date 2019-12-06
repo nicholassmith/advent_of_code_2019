acbd41b9a0dccf3c1e6b2e51081c370d91f93624
Nick Smith <nsmith@amplience.com>
2019-12-06 11:15:37 +0000

    Part 2 solution, some fun with slices

--- advent_day_2/main.go
+++ advent_day_2/main.go
@@ -26,20 +26,45 @@ func main() {
 		positions = append(positions, x)
 	}
 
-	fmt.Printf("part 1: %d\n", compute(positions))
+	part1pos := make([]int, len(positions))
+	part2pos := make([]int, len(positions))
+
+	copy(part1pos, positions)
+	copy(part2pos, positions)
+
+	fmt.Printf("part 1: %d\n", compute(part1pos))
+	fmt.Printf("part 2: %d\n", part2(part2pos))
+}
+
+func part2(positions []int) int {
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			clone := make([]int, len(positions))
+			copy(clone, positions)
+			clone[1] = noun
+			clone[2] = verb
+			if compute(clone) == 19690720 {
+				return (100 * noun) + verb
+			}
+		}
+	}
+
+	return 0
 }
 
 func compute(positions []int) int {
 	for i := 0; i < len(positions); i += 4 {
 		opcode := positions[i]
-
+		pos1 := positions[i+1]
+		pos2 := positions[i+2]
+		store := positions[i+3]
 		switch opcode {
 		case 1:
-			res := positions[positions[i+1]] + positions[positions[i+2]]
-			positions[positions[i+3]] = res
+			res := positions[pos1] + positions[pos2]
+			positions[store] = res
 		case 2:
-			res := positions[positions[i+1]] * positions[positions[i+2]]
-			positions[positions[i+3]] = res
+			res := positions[pos1] * positions[pos2]
+			positions[store] = res
 		case 99:
 			return positions[0]
 		default:
